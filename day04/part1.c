feb13cd3439cf7ae7abeeaecc34bf22c835fca78
hvdncamp <Heidi.VandenCamp@UGent.be>
2022-12-04 20:17:05 +0100

    solve 4

---
+++ day04/part1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../lib/general_functions.h"
+
+int main(int argc, char ** argv) {
+    FILE * file = fopen(argv[3], "r");
+    int start1, end1, start2, end2;
+    int pair_count = 0;
+    while(fscanf(file, "%d-%d,%d-%d",&start1, &end1, &start2, &end2) == 4){
+        next_character(file);//newline
+        if((start1 <= start2 && end1 >= end2) || (start1 >= start2 && end1 <= end2)) {
+            pair_count++;
+        }
+
+    }
+    printf("There are %d such pairs\n", pair_count);
+    return 0;
+}
