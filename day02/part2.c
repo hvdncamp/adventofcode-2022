4d6b03866cdc4ded77a109209465e1c76e793d61
hvdncamp <heidi.vandencamp@ugent.be>
2022-12-05 18:25:10 +0100

    input day 5

--- day02/part2.c
+++ day02/part2.c
@@ -4,15 +4,13 @@ int score(int opponent, int result){
     switch (result) {
         case 0:
             return (opponent + 2) % 3 + 1;
-            break;
         case 1:
             return opponent + 4;
-            break;
         case 2:
             return (opponent + 1) % 3 + 7;
-            break;
         default:
-            break;
+            fprintf(stderr, "%d is not a valid value for result in function 'score'", result);
+            return 0;
     }
 }
 
